1ab6d1802eb5803ea43ee55473298c1a51a90541
gamzenurdemir <demirgamzenn@gmail.com>
2023-07-14 13:48:20 +0300

    task-3 remove unnecessary files

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -7,13 +7,11 @@ builder.Services.AddScoped<IExampleScopedService,ExampleScopedService>();
 builder.Services.AddTransient<IExampleTransientService,ExampleTransientService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
